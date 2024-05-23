8d9569179c02fdab2c21f841a8a5ba371571ab86
DiegoGomez <diegocor4@gmail.com>
2024-05-22 23:21:48 -0300

    ejercicio 01 en proceso

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/DiegoGomezcor4.py
@@ -0,0 +1,42 @@
+"""
+ * EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */
+ """
+
+# aritmeticos
+
+print('suma: 2 + 2' )
+print(2 + 2)
+
+print('resta: 3 - 2: ')
+print( 3 - 2 )
+
+print('multiplicacion: 3 * 2')
+print(3 * 2)
+
+print('division: 10 / 2')
+print(10 / 2)
+
+print('modulo: 6 % 3')
+print(6 % 3)
+
+print('potencia: 5 ** 3')
+print(5 ** 3)
+
+
+
+
